b7ca776555a1361bc7640c4f67b005a80e5e36be
okun915 <82748855+okun915@users.noreply.github.com>
2023-06-20 10:52:12 +0800

    Delete MEA_heat_cal.m

--- MEA_heat_cal.m
+++
@@ -1,6 +0,0 @@
-function stack_T = MEA_heat_cal(heat_produced,pip_Q)
-
-stack_mea_cp =  870; % [J/(kg*K)] Overall specific heat of membrane electrode assembly
-stack_T = (heat_produced-pip_Q)/stack_mea_cp;
-
-end
